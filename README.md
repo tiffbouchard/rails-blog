7761bc06d773a7e58fb292b2d56dceb895831f20
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-12-10 17:27:41 -0500

    add delete functionality and form partial

--- README.md
+++ README.md
@@ -3,14 +3,21 @@ Rails blog 🥴
 just to get familiar with ruby on rails
 
 ruby version: `ruby 2.7.2`
+
 extra dependencies:
 `gem 'better_errors', '~> 2.9', '>= 2.9.1'`
+
 `gem 'guard', '~> 2.16', '>= 2.16.2'`
+
 `gem 'guard-livereload', '~> 2.5', '>= 2.5.2'`
+
 `gem 'bulma-rails', '~> 0.9.1'`
+
 `gem 'simple_form', '~> 5.0', '>= 5.0.3'`
 
 to start:
 `bundle`
+
 `rails db:migrate`
+
 `rails s`
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,28 @@ class PostsController < ApplicationController
     end
 
 
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to @post 
+        else
+            render 'edit'
+        end
+    end
+
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
+    end
+
 
     private
 
